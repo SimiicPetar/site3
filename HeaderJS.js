9110cbf5c8c7fe5ff6c2d85fb2a10a7e11a0fbfc
Simic <38137063+SimiicPetar@users.noreply.github.com>
2019-06-04 05:41:41 +0200

    Add files via upload

---
+++ HeaderJS.js
@@ -0,0 +1,99 @@
+
+var i =0;
+		var images = [];
+		var time = 3000;
+
+		images[0]='https://hashtelegraph.com/wp-content/uploads/2018/02/blockchain.jpg';
+		images[1]='https://ak5.picdn.net/shutterstock/videos/12424295/thumb/1.jpg';
+		
+
+		function changeImg(){
+			document.slide.src = images[i];
+
+			if(i < images.length - 1){
+				i++;
+			} else{
+				i=0;
+			}
+
+			setTimeout("changeImg()", time);
+
+		}
+		window.onload = changeImg;
+
+		
+		//img name slide
+		//header sticky
+		//slike izbacuje hover
+		//slike
+		//jqurey
+		//button
+		
+
+		/*	$(".about").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#text2").offset().top},
+        'slow');
+});
+		$(".how").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#h5").offset().top},
+        'slow');
+});
+		$(".ccc").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#Contant").offset().top},
+        'slow');
+});*/
+//JQUERY uvecava
+
+ /* $(".about").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#text2").offset().top},
+        'slow');
+});
+		$(".how").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#h5").offset().top},
+        'slow');
+});
+		$(".ccc").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#Contant").offset().top},
+        'slow');
+}); */
+
+//naslo tekst 1redslike 2redslike buuton1 button2 jqureyUvecava 
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////
+
+	/* Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML.[1] CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.
+
+
+	Alongside HTML and CSS, JavaScript is one of the core technologies of the World Wide Web.[9] JavaScript enables interactive web pages and is an essential part of web applications. The vast majority of websites use it,[10] and major web browsers have a dedicated JavaScript engine to execute it.
+
+	Angular (commonly referred to as "Angular 2+" or "Angular v2 and above")[4][5] is a TypeScript-based open-source web application framework led by the Angular Team at Google and by a community of individuals and corporations. Angular is a complete rewrite from the same team that built AngularJS.
+
+	Bootstrap is a free and open-source front-end web framework. It contains HTML and CSS-based design templates for typography, forms, buttons, navigation and other interface components, as well as optional JavaScript extensions. Unlike many earlier web frameworks, it concerns itself with front-end development only.
+
+
+	jQuery is a JavaScript library designed to simplify HTML DOM tree traversal and manipulation, as well as event handling, CSS animation, and Ajax.[3] It is free, open-source software using the permissive MIT License.[2] Web analysis (from 2017) indicates that it is the most widely deployed JavaScript library by a large margin.[
+
+
+
+
+
+
+
+	*/
